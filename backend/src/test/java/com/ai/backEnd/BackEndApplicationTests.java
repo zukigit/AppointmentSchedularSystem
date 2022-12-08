e700289df25d97e02deb0ea038fada9dd888012b
zuki <zuki@nine.com>
2022-12-08 11:07:32 +0630

    add backend

---
+++ backend/src/test/java/com/ai/backEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
