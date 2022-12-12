a82bf9d74c635ef81c13dfed28ba6d718751daf6
Ye Yint Swe <itsyeyintswe.com>
2022-12-12 15:50:33 +0630

    sidebar update yys

--- frontEnd/src/app/components/sidebar/sidebar.component.ts
+++ frontEnd/src/app/components/sidebar/sidebar.component.ts
@@ -11,11 +11,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
     { path: '/user-details', title: 'User Details',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    
 ];
 
 @Component({
