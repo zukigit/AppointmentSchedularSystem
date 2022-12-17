e1b9efc3e81be2c087477e99a68723175546abc4
zuki <zuki@nine.com>
2022-12-17 21:35:49 +0630

    to call method for choe gyi

--- frontEnd/src/app/services/logout.service.ts
+++ frontEnd/src/app/services/logout.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogoutService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  logout() {
+    localStorage.removeItem("jwtToken");
+    this.router.navigate(['/']);
+  }
 }
