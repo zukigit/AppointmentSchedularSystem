8db60d5377672768c180d4da048d41e23f045581
zuki <zuki@nine.com>
2022-12-30 15:37:02 +0630

    update security

--- frontEnd/src/app/admin-auth.guard.ts
+++ frontEnd/src/app/admin-auth.guard.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthGuard implements CanActivate {
   userRole: string = localStorage.getItem("loggedInUserRole");
+  token: string = localStorage.getItem("jwtToken");
+  jwtHelper: JwtHelperService = new JwtHelperService();
   constructor(private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if(this.userRole == "ROLE_ADMIN") {
-      return true;
-    } else {  
-      this.router.navigate(['/login']).then(()=>{
-        window.location.reload();
-      }); 
-    }
+      if(this.userRole == "ROLE_ADMIN" && !this.jwtHelper.isTokenExpired(this.token)) {
+        return true;
+      } else {
+        alert("log in again");  
+        this.router.navigate(['/login']).then(()=>{
+          window.location.reload();
+        }); 
+      }
   }
   
 }
