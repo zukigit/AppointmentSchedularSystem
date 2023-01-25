8e44f70a5a2fca997b6994df0ced899c3c3d09a9
unknown <minphonekhant2703@gmail.com>
2023-01-25 14:50:00 +0630

    noti 90%

--- frontEnd/src/app/components/navbar/navbar.component.ts
+++ frontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 // import { ROUTES } from '../sidebar/sidebar.component';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Router } from '@angular/router';
 import { NotiService } from 'app/services/noti.service';
 import { ShowAppointment } from 'app/model/show-appointment';
@@ -17,59 +17,70 @@ declare interface RouteInfo {
     icon: string;
     class: string;
 }
-export const ROUTES= [
+export const ROUTES = [
     // { path: 'admin/user-profile', component: UserProfileComponent },
-{ path: '#', title: 'Log Out',  icon: 'logout', class: '' },
-    
-    
-    
+    { path: '#', title: 'Log Out', icon: 'logout', class: '' },
+
+
+
 ];
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
     // menuItems: any[];
     private listTitles: any[];
     location: Location;
-      mobile_menu_visible: any = 0;
+    mobile_menu_visible: any = 0;
     private toggleButton: any;
     private sidebarVisible: boolean;
     appointment: ShowAppointment = new ShowAppointment();
-    
-    bindData: any;
+
+    bindData: any[] = [];
     temData: any;
     readedNoti: number;
 
     // notificationCount$ = this.notiService.notificationCount$;
 
-    constructor(location: Location,  private element: ElementRef, private router: Router,private notiService : NotiService) {
-      this.location = location;
-          this.sidebarVisible = false;
+    constructor(location: Location, private element: ElementRef, private router: Router, private notiService: NotiService) {
+        this.location = location;
+        this.sidebarVisible = false;
 
-          
+
     }
-    notiModel : NotiModel;
-    dataByNoti : any=[];
+    notiModel: NotiModel;
+    dataByNoti: any = [];
     Notis: any
-    notiArray: any [];
-    loginId : string;
-    user : User = new User();
-    // text : string;
+    notiArray: any[];
+    loginId: string;
+    user: User = new User();
+    text: string;
+    showData: any[] = [];
 
-    unreadNoti :number;
+    unreadNoti: number;
 
     realTimeDataSubscription$: Subscription;
 
-    
-    ngOnInit(){
+
+    ngOnInit() {
         this.unreadNoti = Number(localStorage.getItem("unreadNoti"));
         this.loginId = localStorage.getItem("loggedInUserId");
         this.readedNoti = Number(localStorage.getItem("totalNoti"));
-        this.getNotiFirstTime();
-        this.realTimeData();
+        setTimeout(() => {
+            this.getNotiFirstTime();
+
+        }, 1200);
+
+        setTimeout(() => {
+            this.realTimeData();
+        }, 1000);
+
+        //this.realTimeData();
         //  this.getNotiByType();
+
+        console.log(this.bindData)
 
         this.listTitles = ROUTES.filter(listTitle => listTitle);
         const navbar: HTMLElement = this.element.nativeElement;
@@ -78,65 +89,78 @@ export class NavbarComponent implements OnInit {
             this.sidebarClose();
             var $layer: any = document.getElementsByClassName('close-layer')[0];
             if ($layer) {
-            $layer.remove();
-            this.mobile_menu_visible = 0;
+                $layer.remove();
+                this.mobile_menu_visible = 0;
             }
         });
     }
 
     removeReadCount() {
         localStorage.setItem("unreadNoti", '0');
-         this.unreadNoti = 0;
+        this.unreadNoti = 0;
     }
 
     realTimeData() {
         this.realTimeDataSubscription$ = timer(0, 100)
-          .pipe(switchMap(_ => this.notiService.getNoti(this.loginId)))
-          .subscribe(res => {
-            this.temData = res;
-            if(this.temData.length > this.readedNoti) {
-                this.unreadNoti = this.temData.length - this.readedNoti;
-                this.bindData = res;
-                this.readedNoti = this.temData.length;
-                localStorage.setItem("unreadNoti", `${this.unreadNoti}`);
-                localStorage.setItem("totalNoti", `${this.readedNoti}`);
-            }
-        });
+            .pipe(switchMap(_ => this.notiService.getNoti(this.loginId)))
+            .subscribe(res => {
+                this.temData = res;
+                if (this.temData.length > this.readedNoti) {
+                    this.unreadNoti = this.temData.length - this.readedNoti;
+                    this.bindData.push(res);
+                    this.readedNoti = this.temData.length;
+                    localStorage.setItem("unreadNoti", `${this.unreadNoti}`);
+                    localStorage.setItem("totalNoti", `${this.readedNoti}`);
+                }
+            });
     }
 
     getNotiFirstTime() {
         this.notiService.getNoti(this.loginId).subscribe(
             (data) => {
-                let mydata ; 
+                let mydata;
 
                 console.log("data" + data);
-                mydata=this.bindData=data;
-                for(var noti of mydata){
-          
-                if(this.bindData.notiType = "CREATE_APP"){
-                   this.bindData.text = "created the appointment :"
-                   console.log(this.bindData)
-                }else if(this.bindData.notiType = "EDIT_APP"){
-                    this.bindData.text="edited the appointment :"
-                }else if(this.bindData.notiType = "DELETE_APP")
-                    this.bindData.text="deleted the appointment :"
-            
-        
-                console.log(this.bindData.length + "amount")
-           }
-        }
+                // mydata=
+                mydata = data;
+                // mydata=data
+                for (var noti of mydata) {
+                    if
+                        (noti.notiType == "CREATE_APP") {
+                        this.bindData.push(noti.createUser.name + " created the appointment :" + noti.title)
+                        console.log("create app " + this.showData)
+                    }
+                    else if (noti.notiType == "EDIT_APP") {
+                        this.bindData.push(noti.createUser.name + " edited the appointment :" + noti.title);
+                    }
+                    else if (noti.notiType == "DELETE_APP") {
+                        this.bindData.push(noti.createUser.name + " deleted the appointment :" + noti.title);
+                    }
+                    else if(noti.notiType == "USER_REMOVED" && noti.employee_id == this.loginId){
+                        this.bindData.push("You are removed from the appointment!");
+                    }
+                    else if(noti.notiType == "USER_ADD" && noti.employee_id == this.loginId){
+                        this.bindData.push("You are added the appointment :" + noti.title);
+                    }
+                }
+                for (let x of this.bindData) {
+                    console.log(x + "data")
+
+                }
+                // this.bindData.push(this.showData)
+                console.log(this.bindData.length + "length")
+            }
         )
-        
     }
 
-    goToDashboard(){
+    goToDashboard() {
         this.router.navigate(['admin/dashboard'])
     }
 
     sidebarOpen() {
         const toggleButton = this.toggleButton;
         const body = document.getElementsByTagName('body')[0];
-        setTimeout(function(){
+        setTimeout(function () {
             toggleButton.classList.add('toggled');
         }, 500);
 
@@ -166,13 +190,13 @@ export class NavbarComponent implements OnInit {
             if ($layer) {
                 $layer.remove();
             }
-            setTimeout(function() {
+            setTimeout(function () {
                 $toggle.classList.remove('toggled');
             }, 400);
 
             this.mobile_menu_visible = 0;
         } else {
-            setTimeout(function() {
+            setTimeout(function () {
                 $toggle.classList.add('toggled');
             }, 430);
 
@@ -182,22 +206,22 @@ export class NavbarComponent implements OnInit {
 
             if (body.querySelectorAll('.main-panel')) {
                 document.getElementsByClassName('main-panel')[0].appendChild($layer);
-            }else if (body.classList.contains('off-canvas-sidebar')) {
+            } else if (body.classList.contains('off-canvas-sidebar')) {
                 document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 $layer.classList.add('visible');
             }, 100);
 
-            $layer.onclick = function() { //asign a function
-              body.classList.remove('nav-open');
-              this.mobile_menu_visible = 0;
-              $layer.classList.remove('visible');
-              setTimeout(function() {
-                  $layer.remove();
-                  $toggle.classList.remove('toggled');
-              }, 400);
+            $layer.onclick = function () { //asign a function
+                body.classList.remove('nav-open');
+                this.mobile_menu_visible = 0;
+                $layer.classList.remove('visible');
+                setTimeout(function () {
+                    $layer.remove();
+                    $toggle.classList.remove('toggled');
+                }, 400);
             }.bind(this);
 
             body.classList.add('nav-open');
@@ -206,18 +230,18 @@ export class NavbarComponent implements OnInit {
         }
     };
 
-    getTitle(){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      if(titlee.charAt(0) === '#'){
-          titlee = titlee.slice( 1 );
-      }
+    getTitle() {
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+        if (titlee.charAt(0) === '#') {
+            titlee = titlee.slice(1);
+        }
 
-      for(var item = 0; item < this.listTitles.length; item++){
-          if(this.listTitles[item].path === titlee){
-              return this.listTitles[item].title;
-          }
-      }
-      return 'Dashboard';
+        for (var item = 0; item < this.listTitles.length; item++) {
+            if (this.listTitles[item].path === titlee) {
+                return this.listTitles[item].title;
+            }
+        }
+        return 'Dashboard';
     }
     logout() {
         localStorage.removeItem("jwtToken");
@@ -225,8 +249,8 @@ export class NavbarComponent implements OnInit {
         localStorage.removeItem("loggedInUserId");
         localStorage.removeItem("loggedInUserRole");
 
-        this.router.navigate(['/login']).then(()=>{
+        this.router.navigate(['/login']).then(() => {
             window.location.reload();
         });
-      }
+    }
 }
