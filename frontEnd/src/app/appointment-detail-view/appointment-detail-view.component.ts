06d3afdebecc0e75ffa4fe31d80577db0ea0ac0e
zuki <zuki@nine.com>
2023-01-26 10:45:55 +0630

    add privacy to appointments

--- frontEnd/src/app/appointment-detail-view/appointment-detail-view.component.ts
+++ frontEnd/src/app/appointment-detail-view/appointment-detail-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { AppointmentService } from 'app/services/appointment.service';
 import { UserService } from 'app/services/user.service';
 import { data } from 'jquery';
@@ -17,62 +17,77 @@ export class AppointmentDetailViewComponent implements OnInit {
   employee = [];
   urls = [];
 
-  constructor(private route: ActivatedRoute,private appService:AppointmentService, private userService:UserService) { }
+  constructor(private route: ActivatedRoute,private appService:AppointmentService, private userService:UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.appService.viewOnlyAppointmentById(this.id).subscribe(
       (res : any) => {
-        this.res = res;
-        this.files = res.files;
-        this.employee = res.employee;
-        this.generatePhotos();
+        if(res.type != "PUBLIC") {
+          this.appService.checkUserInclude(this.id, res.appointment_id).subscribe(
+            (data: any) => {
+              if (data) {
+                this.res = res;
+                this.files = res.files;
+                this.employee = res.employee;
+                this.generatePhotos();
+              }
+            }, error => {
+              alert("this appointment is private and you are not in there")
+              this.router.navigate(['/admin/dashboard']);
+            }
+          );
+        } else {
+          this.res = res;
+          this.files = res.files;
+          this.employee = res.employee;
+          this.generatePhotos();
+        }
       },
       error => console.log("get app error " + error));
-    }
+  }
 
-    //update
-    updateApp() {
-      alert("Ok nrr sarrr")
-    }
-    //delete
-    deleteApp(){
-      alert("Okayy")
-    }
+  //update
+  updateApp() {
+    alert("Ok nrr sarrr")
+  }
+  //delete
+  deleteApp(){
+    alert("Okayy")
+  }
 
-    downloadFile(file) {      
-      this.appService.fileDownload(file.file_id).subscribe(
-        data => {
-          let blob = new Blob([data], { type: file.type});
-          let downloadLink = document.createElement('a');
-          downloadLink.href = window.URL.createObjectURL(blob);
-          if (file.file_name)
-                downloadLink.setAttribute('download', file.file_name);
-          document.body.appendChild(downloadLink);
-          downloadLink.click();
-        }
-      )
-    }
+  downloadFile(file) {      
+    this.appService.fileDownload(file.file_id).subscribe(
+      data => {
+        let blob = new Blob([data], { type: file.type});
+        let downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(blob);
+        if (file.file_name)
+              downloadLink.setAttribute('download', file.file_name);
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+      }
+    )
+  }
 
-    generatePhotos() {
-      for(let i = 0 ; i < this.employee.length; i++) {
-        if(this.employee[i].userImage == null) {
-          this.employee[i].userImage = new Image();
-          this.employee[i].userImage.data = "./assets/img/default.jpg";
-        } else {
-          this.userService.getImage(this.employee[i].employee_id).subscribe(response => {
-            const reader = new FileReader();
-            reader.addEventListener('load', () => {
-              this.employee[i].userImage.data = reader.result;
-            }, false);
-            if (response) {
-              reader.readAsDataURL(response);
-            } else{
-              this.employee[i].userImage.data = "./assets/img/default.jpg";
-            }
-          });
-        }
+  generatePhotos() {
+    for(let i = 0 ; i < this.employee.length; i++) {
+      if(this.employee[i].userImage == null) {
+        this.employee[i].userImage = new Image();
+        this.employee[i].userImage.data = "./assets/img/default.jpg";
+      } else {
+        this.userService.getImage(this.employee[i].employee_id).subscribe(response => {
+          const reader = new FileReader();
+          reader.addEventListener('load', () => {
+            this.employee[i].userImage.data = reader.result;
+          }, false);
+          if (response) {
+            reader.readAsDataURL(response);
+          } else{
+            this.employee[i].userImage.data = "./assets/img/default.jpg";
+          }
+        });
       }
     }
-  
+  }
 }
