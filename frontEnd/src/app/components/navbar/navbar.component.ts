330eaea6fa7f2f6068e46e9b328e58768bd44fcf
zuki <zuki@nine.com>
2023-01-27 15:01:45 +0630

    noti fixed final

--- frontEnd/src/app/components/navbar/navbar.component.ts
+++ frontEnd/src/app/components/navbar/navbar.component.ts
@@ -102,6 +102,7 @@ export class NavbarComponent implements OnInit {
             },
             error=> console.log("error " + error.message)
         );
+        this.checkNotiCounts();
         this.router.navigate(['/admin/appointment_detail_view', appId]).then (() => window.location.reload()),10000;;
     }
 
