2a34c1b8958552f33283c7385af42d2b3ec0f3f5
unknown <chueyamonenaing@gmail.com>
2023-02-01 11:30:51 +0630

    Final Jasper

--- frontEnd/src/app/admin_role/dashboard/dashboard.component.ts
+++ frontEnd/src/app/admin_role/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ import { DatePipe } from '@angular/common';
 import * as e from 'express';
 import { ReportService } from 'app/services/report.service';
 import { co } from '@fullcalendar/core/internal-common';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -116,7 +117,7 @@ export class DashboardComponent implements OnInit {
 
 
     this.getSchedulesById();
-    
+
       const dataDailySalesChart: any = {
           labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
           series: [
@@ -201,7 +202,7 @@ export class DashboardComponent implements OnInit {
             this.showDataApp =data
       })
 
-    
+
 
         this.appService.getAppointmentById(this.loginId).subscribe(data =>
           this.showDataApp = data);
@@ -213,7 +214,13 @@ export class DashboardComponent implements OnInit {
   getAllApp(){
 
     this.reportService.getAllApp(this.loginId).subscribe(data =>
-      console.log("report data"+data)
+      console.log("report data"+data),
+      Swal.fire({
+        icon: 'success',
+        title: 'Successfully Saved ',
+        text: 'TodayAppointment is saved on Desktop',
+      })
+
      );
 
 
